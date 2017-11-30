615ba40189a2ddc29e928d98c3bdd483c2d140a9
DESKTOP-OTGB2GG\ASUS <sc920930@qq.com>
2017-11-30 14:34:34 +0800

    fixed MusicPlayer

--- javascripts/music_player.js
+++ javascripts/music_player.js
@@ -1,32 +1,34 @@
-import { ProgressBar } from './progress_bar.js'
-import { LyricsPlayer } from './lyrics_player.js'
-import { songUrl, lyricsUrl, albumCoverUrl } from './helpers.js'
+import {
+  ProgressBar
+} from './progress_bar.js'
+import {
+  LyricsPlayer
+} from './lyrics_player.js'
+import {
+  songUrl,
+  lyricsUrl,
+  albumCoverUrl
+} from './helpers.js'
 
-class MiniPlayer{
-  constructor(el){
+class MiniPlayer {
+  constructor(el) {
     this.$el = el;
   }
-  showMessage(imgUrl,Title){
+  showMessage(imgUrl, Title) {
     this.$el.querySelector('.songimg').style.background = `url(${imgUrl}) 0/cover`
     this.$el.querySelector('.song-message>.songtitle').textContent = Title;
   }
-  PlayerHandler(e){
-   switch(e.target.className){
-    case "icon-action icon-play": 
-    if(!this.$el.querySelector('.icon-action.icon-play'))return;
-    this.$el.querySelector('.icon-action.icon-play').classList.replace('icon-play','icon-pause')
-    this.showlyrics()
-    break;
-    case "icon-action icon-pause": 
-    this.$el.querySelector('.icon-action.icon-pause').classList.replace('icon-pause','icon-play')
-    this.showlyrics()
-    break;
-    case "songList":
-    break;
-    default: 
-   } 
+  PlayerHandler(target) {
+    switch (true) {
+      case target.matches('.icon-play'):
+        this.$el.querySelector('.icon-action.icon-play').classList.replace('icon-play', 'icon-pause')
+        break
+      case target.matches('.icon-pause'):
+        this.$el.querySelector('.icon-action.icon-pause').classList.replace('icon-pause', 'icon-play')
+        break
+    }
   }
-  showlyrics(){
+  showlyrics() {
 
   }
 }
@@ -58,12 +60,14 @@ export class MusicPlayer {
 
   onOff(event) {
     let target = event.target
+    if (!this.contains(event.target))
+      target = this.$el.querySelector('.' + event.target.classList[0])
     switch (true) {
       case target.matches('.icon-play'):
-        this.onPlay(event)
+        this.onPlay(target)
         break
       case target.matches('.icon-pause'):
-        this.onPause(event)
+        this.onPause(target)
         break
       case target.matches('.icon-list'):
         this.hide()
@@ -71,48 +75,49 @@ export class MusicPlayer {
     }
   }
 
-  onPlay(event) {
-    if (this.fetching||!this.songid) return
+  onPlay(target) {
+    if (this.fetching || !this.songid) return
     this.$audio.play()
     this.lyrics.start()
     this.progress.start()
-    this.miniPlayer.PlayerHandler(event)
-    event.target.classList.add('icon-pause')
-    event.target.classList.remove('icon-play')
+    this.miniPlayer.PlayerHandler(target)
+    target.classList.add('icon-pause')
+    target.classList.remove('icon-play')
   }
 
-  onPause(event) {
+  onPause(target) {
     this.$audio.pause()
     this.lyrics.pause()
     this.progress.pause()
-    this.miniPlayer.PlayerHandler(event)
-    event.target.classList.add('icon-play')
-    event.target.classList.remove('icon-pause')
+    this.miniPlayer.PlayerHandler(target)
+    target.classList.add('icon-play')
+    target.classList.remove('icon-pause')
   }
-  onPosition(e){
+  onPosition(e) {
     // console.log(e.target.style.transform.replace(/translate\((.+?)\)/g,'$1'))
-    let clickBarPosition =  e.clientX-this.$el.querySelector('.progress-elapsed').offsetWidth,
-        ratio = clickBarPosition/e.target.clientWidth;
-        this.$audio.currentTime = this.$audio.duration*ratio
-        this.progress.elapsed=ratio*this.progress.duration
-        this.progress.update(true)
-        this.lyrics.update();
+    let clickBarPosition = e.clientX - this.$el.querySelector('.progress-elapsed').offsetWidth,
+      ratio = clickBarPosition / e.target.clientWidth;
+    this.$audio.currentTime = this.$audio.duration * ratio
+    this.progress.elapsed = ratio * this.progress.duration
+    this.progress.update(true)
+    this.lyrics.update();
   }
   play(options = {}) {
     if (!options) return
     let coverUrl = albumCoverUrl(options.albummid),
-        artist = options.artist;
+      artist = options.artist;
     this.$el.querySelector('.song-name').textContent = options.songname
-    this.$el.querySelector('.song-artist').textContent= artist
+    this.$el.querySelector('.song-artist').textContent = artist
     this.progress.reset(options.duration)
     this.$el.querySelector('.album-cover').src = coverUrl
     this.$el.querySelector('.player-background').style.backgroundImage = `url(${coverUrl})`
-    this.miniPlayer.showMessage(coverUrl,artist)
+    this.miniPlayer.showMessage(coverUrl, artist)
     if (options.songid) {
       if (this.songid !== options.songid) {
         this.$el.querySelector('.icon-action').className = 'icon-action icon-play'
+        this.miniPlayer.$el.querySelector('.icon-action').className = 'icon-action icon-play'
       }
-      
+
       this.songid = options.songid
       this.$audio.src = songUrl(this.songid)
       this.fetching = true
@@ -124,7 +129,7 @@ export class MusicPlayer {
         .then(() => this.fetching = false)
     }
     this.show()
-    this.progress.$el.querySelector('.progress-bar').addEventListener('click',this.onPosition.bind(this))
+    this.progress.$el.querySelector('.progress-bar').addEventListener('click', this.onPosition.bind(this))
   }
 
   show() {
@@ -135,5 +140,26 @@ export class MusicPlayer {
   hide() {
     this.$el.classList.remove('show')
     document.body.classList.remove('noscroll')
+  }
+  contains(node) {
+    if($el.contains){
+      return $el.contains(node) 
+    }
+    let $el = this.$el,
+    queue = [$el],
+    i = 0;
+    while (queue.length > 0) {
+      let currentNode = queue.shift()
+      for (; i < $el.children.length; i++) {
+        let boundary = $el.children.length - 1;
+        currentNode = $el.children[i]
+        if (currentNode !== node) {
+          queue.push(node)
+          if (i === boundary)
+            $el = queue.shift()
+        } else return true
+      }
+    }
+    return false
   }
 }
